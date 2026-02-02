f937d983d3d0edb9c2f5596ceab2ff809369e633
kirmada102 <geetppatil3000@gmail.com>
2026-02-02 15:07:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ import Level2 from "./scenes/Level2.js";
 import Level3 from "./scenes/Level3.js";
 import FinalCinematic from "./scenes/FinalCinematic.js";
 
-new Phaser.Game({
+const config = {
   type: Phaser.AUTO,
   width: 960,
   height: 540,
+  backgroundColor: "#0e0e1a",
   physics: {
     default: "arcade",
-    arcade: { gravity: { y: 900 }, debug: false }
+    arcade: {
+      gravity: { y: 900 },
+      debug: false
+    }
   },
   scene: [Level1, Level2, Level3, FinalCinematic]
-});
+};
+
+// 🐴 Create the game
+const game = new Phaser.Game(config);
+
+// 🐴 FORCE Phaser to start the gameplay scene
+game.scene.start("Level1");
